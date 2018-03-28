f1508eb349e0b3ef1e8f02f6821f3444c76c155f
Huguy <hugo.simoes_maricato@insa-rouen.fr>
2018-03-28 23:46:12 +0200

    Création d'une DataBase activity qui se lancera lors du lancement de l'appli et qui sera notre menu de chargement et en meme temps l'endroit ou nous créons la DB

--- app/src/main/java/com/example/mots/DataBaseActivity.java
+++ app/src/main/java/com/example/mots/DataBaseActivity.java
@@ -1,12 +1,20 @@
 package com.example.mots;
 
 import android.app.Activity;
+import android.os.Bundle;
+
 
 public class DataBaseActivity  extends Activity {
 
-    public MotsDAO buildDb(){
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         AppDataBase db = AppDataBase.getAppDatabase(getApplicationContext());
-        MotsDAO dbDao = db.MotsDao();
+        MotsDAO dbDAO = db.MotsDao();
+        buildDb(dbDAO);
+    }
+
+
+    private void buildDb(MotsDAO dbDAO){  //créé la DB
         System.out.println("Importing database");
         String[] namesList = getNamesOfTheLists();
         int idList = 1;
@@ -21,7 +29,6 @@ public class DataBaseActivity  extends Activity {
             }
             idList ++;
         }
-        return dbDAO;
     }
 
     private void insertWordInDB (Mots aWord, MotsDAO dbDAO) {
@@ -33,18 +40,18 @@ public class DataBaseActivity  extends Activity {
         return namesList;
     }
 
-    private String[] getList(String nameList){
+    private String[] getList(String nameList){ //Récupère la liste associé au nom fourni
         int listID = getResources().getIdentifier(nameList, "array", getPackageName());
         String[] WordsList = getResources().getStringArray(listID);
         return WordsList;
     }
 
-    private String[] separateWords (String linkedWords) {
+    private String[] separateWords (String linkedWords) { //sépare les mots français collé aux mots anglais
         String[] separatedWords = linkedWords.split("/");
         return separatedWords;
     }
 
-    private Mots setWord (String[] separatedWords, int idList, int idInList) {
+    private Mots setWord (String[] separatedWords, int idList, int idInList) { //Permet de définir le mot dans la DB avec ses ID
         Mots aWord = new Mots(idList, idInList, separatedWords[0], separatedWords[1], 0, 0);
         return aWord;
     }
