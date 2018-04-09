ef18199ddd35c1ab61ff74cf68e33a059e172edb
Huguy <hugo.simoes_maricato@insa-rouen.fr>
2018-04-09 10:33:17 +0200

    Test d'implémentation SpeechToText

--- app/src/main/java/com/souillard/Activities/MainMenu.java
+++ app/src/main/java/com/souillard/Activities/MainMenu.java
@@ -1,15 +1,28 @@
 package com.souillard.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.souillard.SpeechToText.SpeechtoTextActivity;
 
 import com.souillard.R;
 
 
 public class MainMenu extends Activity {
 
+    private Intent intent = null;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
     }
+
+    public void onScreenClick(View view) {
+
+        intent = new Intent(this, SpeechtoTextActivity.class);
+        startActivity(intent);
+    }
+
 }
