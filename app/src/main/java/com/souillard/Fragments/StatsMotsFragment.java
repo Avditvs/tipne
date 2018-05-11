4a0de23e793f6e8ab1f34613bc3c12fad7542056
Louis Germain <germain.louis.80@gmail.com>
2018-05-11 22:49:38 +0200

    Création de l'activité de stats et des fregments des MotsStats et VerbesStats

---
+++ app/src/main/java/com/souillard/Fragments/StatsMotsFragment.java
@@ -0,0 +1,28 @@
+package com.souillard.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.souillard.R;
+
+public class StatsMotsFragment extends Fragment {
+
+    public  StatsMotsFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stats_mots, container, false);
+    }
+
+}
