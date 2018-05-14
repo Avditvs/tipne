2d3c55807e44c3043e6f3867bea3e864b4d7e0c8
Huguy <hugo.simoes_maricato@insa-rouen.fr>
2018-05-14 16:06:15 +0200

    Suppression fichiers inutiles + réorganisation du menu principal.

--- app/src/main/java/com/souillard/Activities/MainActivity.java
+++ app/src/main/java/com/souillard/Activities/MainActivity.java
@@ -17,35 +17,32 @@ import com.souillard.customViews.MenuButton;
 
 
 public class MainActivity extends Activity {
-    private MenuButton btEntrainement = null;
+    private MenuButton btApprentissage = null;
     private MenuButton btStt = null;
-    private MenuButton btVerbes = null;
     private MenuButton btEval = null;
     private Intent intent = null;
     private Intent intent4 = null;
     private Intent intent2 = null;
     public final static String mode = "";
-    public final static String choixUtilisateur = "";
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //////////////Récupération des views/////////////////
-        btEntrainement = findViewById(R.id.entrainement);
+        btApprentissage = findViewById(R.id.apprentissage);
+        btApprentissage.setOnClickListener(listenerBtApprentissage);
+
         btStt = findViewById(R.id.STT);
-        btEntrainement.setOnClickListener(listenerBtEntrainement);
         btStt.setOnClickListener(listenerBtStt);
-        btVerbes = findViewById(R.id.bt_verbes);
-        btVerbes.setOnClickListener(listenerVerbes);
-        btEval = findViewById(R.id.bt_eval);
-        btEval.setOnClickListener(listenerbtEval);
 
-
-
+        btEval = findViewById(R.id.evaluations);
+        btEval.setOnClickListener(listenerBtEval);
     }
 
-    private View.OnClickListener listenerbtEval = new View.OnClickListener() {
+
+    private View.OnClickListener listenerBtEval = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             intent4 = new Intent(getApplicationContext(), ChooseListActivity.class);
@@ -62,7 +59,7 @@ public class MainActivity extends Activity {
         }
     };
 
-    private View.OnClickListener listenerBtEntrainement = new View.OnClickListener() {
+    private View.OnClickListener listenerBtApprentissage = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
@@ -92,22 +89,5 @@ public class MainActivity extends Activity {
         }
     };
 
-    private View.OnClickListener listenerVerbes = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            final Intent intent3 = new Intent(getApplicationContext(), LearningVerbsActivity.class);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable(){
-                public void run() {
-                    startActivity(intent3);
-                }
-            }, 300);
-        }
-    };
-
-    @Override
-    public void onBackPressed(){
-        //Ne rien faire bite
-    }
 
 }
