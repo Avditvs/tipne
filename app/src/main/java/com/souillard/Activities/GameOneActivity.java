a4bb83ae230a90ac79ee4ff7fd929d518c8f171d
Huguy <hugo.simoes_maricato@insa-rouen.fr>
2018-05-22 16:39:32 +0200

    Continuation GameOne Activity. Manque juste Validate à fixer.

--- app/src/main/java/com/souillard/Activities/GameOneActivity.java
+++ app/src/main/java/com/souillard/Activities/GameOneActivity.java
@@ -2,9 +2,13 @@ package com.souillard.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.souillard.BasesDeDonnées.AppDataBase;
 import com.souillard.BasesDeDonnées.listes.ListesDAO;
 import com.souillard.BasesDeDonnées.mots.Mots;
@@ -25,21 +29,14 @@ public class GameOneActivity extends Activity {
     private ListesDAO listesDAO;
     private MotsDAO motsDAO;
     private List<Mots> mots;
-    private Button lettre1;
-    private Button lettre2;
-    private Button lettre3;
-    private Button lettre4;
-    private Button lettre5;
-    private Button lettre6;
-    private Button lettre7;
-    private Button lettre8;
-    private Button lettre9;
-    private Button lettre10;
-    private Button lettre11;
-    private Button lettre12;
     private Button restart;
     private Button confirm;
     private  Button[] buttons = new Button[12];
+    private TextView motADeviner;
+    private int indice;
+    private int score;
+    private Toast toast;
+    private LinearLayout bodyEval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +48,21 @@ public class GameOneActivity extends Activity {
 
         Bundle extras = getIntent().getExtras();
 
+
         idList = listesDAO.idDeListe((extras.getString("nameList")));
 
         nomListe = findViewById(R.id.ListName);
         nomListe.setText(listesDAO.getProperName(idList));
         mots = motsDAO.getList(idList);
 
+        motADeviner = findViewById(R.id.motadeviner);
+        motADeviner.setText("");
 
         motFr = findViewById(R.id.motFr);
-          //Normalement ça permet de faire un tableau de boutons
+
+
+        restart = findViewById(R.id.restartButton);
+        confirm = findViewById(R.id.confirmationButton);
         buttons[0] = findViewById(R.id.char1);
         buttons[1] = findViewById(R.id.char2);
         buttons[2] = findViewById(R.id.char3);
@@ -72,15 +75,19 @@ public class GameOneActivity extends Activity {
         buttons[9] = findViewById(R.id.char10);
         buttons[10] = findViewById(R.id.char11);
         buttons[11] = findViewById(R.id.char12);
+
+
         Collections.shuffle(mots);
-        //updateNewRandomWord(mots.get(1));
-        int i =0;
-        String test = new String();
-        do {
-            test = mots.get(i).getMotsEn();
-            i++;
-        }while (test.length() > 11);
-        updateView(mots.get(i-1));
+
+        indice =0;
+        updateView(mots.get(getNewWord()));
+
+        for (int j = 0; j < 12; j++){
+            buttons[j].setOnClickListener(clickListenerButton);
+        }
+
+        restart.setOnClickListener(clickListenerRestart);
+        confirm.setOnClickListener(clickListenerValidate);
 
     }
 
@@ -88,6 +95,7 @@ public class GameOneActivity extends Activity {
     private void updateNewRandomWord(Mots mots) {
         motFr.setText(mots.getMotsFr());
     }
+
     private String shuffleWord (String word) {
         List<Character> l = new ArrayList<>();
         for (char c : word.toCharArray()) //for each char of the word selectionned, put it in a list
@@ -103,22 +111,88 @@ public class GameOneActivity extends Activity {
     }
 
 
-
     private void updateView (Mots mots){
 
         updateNewRandomWord(mots);
-        String[] motMelange = shuffleWord(mots.getMotsEn()).split("");
+        String motMelange = shuffleWord(mots.getMotsEn());
 
         int i = 0;
 
-        for (String x : motMelange){
-            buttons[i].setText(x);
+        for (int x = 0; x < motMelange.length(); x++){
+            buttons[x].setText(Character.toString(motMelange.charAt(x)));
             i++;
         }
-        while(i<11) {
+        while(i<12) {
             buttons[i].setText("a");
             i++;
         }
     }
+
+    private View.OnClickListener clickListenerButton = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button boutonCliqué = (Button) v;
+            String lettreBouton = boutonCliqué.getText().toString();
+            String temp = motADeviner.getText().toString();
+            motADeviner.setText(temp + lettreBouton);
+            boutonCliqué.setVisibility(View.INVISIBLE);
+        }
+    };
+
+    private View.OnClickListener clickListenerRestart = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            motADeviner.setText("");
+            for (int k = 0; k < 12; k++){
+                buttons[k].setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+    private int getNewWord(){
+        String test = new String();
+        do {
+            indice++;
+            test = mots.get(indice).getMotsEn();
+        }while (test.length() > 11);
+        return (indice);
+    };
+
+    private View.OnClickListener clickListenerValidate = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            valide();
+        }
+    };
+
+    private void valide(){
+        String motSolution = mots.get(indice).getMotsEn();
+        if (motADeviner.equals(motSolution)) {
+            score++;
+            onRightAnswer();
+        }
+        else {
+            onWrongAnswer();
+        }
+        updateView(mots.get(getNewWord()));
+    };
+
+    private void onRightAnswer(){
+        toast = Toast.makeText(getBaseContext(), "Bonne réponse", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, bodyEval.getBottom()+20);
+        toast.show();
+    };
+
+    private void onWrongAnswer(){
+        String toastText = "Réponse incorrecte, la(les) réponses\ncorrectes étaient :";
+        int i;
+        for (i = 0; i < mots.get(indice).getTabMotsEn().length; i++){
+            toastText = toastText +"\n" + mots.get(indice).getTabMotsEn()[i];
+
+        }
+        toast = Toast.makeText(getBaseContext(), toastText, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, bodyEval.getBottom()+20);
+        toast.show();
+    };
 
 }
