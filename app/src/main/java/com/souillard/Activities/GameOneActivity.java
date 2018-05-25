604d608b9aafb9c712effd00830a947c31641135
cbecarelli <blackpearlpirate@hotmail.fr>
2018-05-25 21:53:16 +0200

    Nettoyage et Commentaire mini-jeu

--- app/src/main/java/com/souillard/Activities/GameOneActivity.java
+++ app/src/main/java/com/souillard/Activities/GameOneActivity.java
@@ -9,7 +9,6 @@ import android.preference.PreferenceManager;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,82 +38,45 @@ public class GameOneActivity extends Activity {
     private List<Mots> mots;
     private Button restart;
     private Button confirm;
-    private  Button[] buttons = new Button[12];
+    private Button[] buttons = new Button[12];
     private int indice;
     private float score;
     private Toast toast;
 
+
+    ///////////////////////////////////////////activité////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_1);
-
-        listesDAO = AppDataBase.getAppDatabase(getApplicationContext()).ListesDAO();
-        motsDAO = AppDataBase.getAppDatabase(getApplicationContext()).MotsDao();
-
-        Bundle extras = getIntent().getExtras();
-
-
-        idList = listesDAO.idDeListe((extras.getString("nameList")));
-
-        nomListe = findViewById(R.id.ListName);
-        nomListe.setText(listesDAO.getProperName(idList));
-        mots = motsDAO.getList(idList);
-
-        motADeviner = findViewById(R.id.motadeviner);
-        motADeviner.setText("");
-
-        chronometer = findViewById(R.id.chronometer);
-        chronometer.setText("c");
-        affichageScore = findViewById(R.id.score);
-        affichageHighscore = findViewById(R.id.highscore);
-        motFr = findViewById(R.id.motFr);
-
-
-        restart = findViewById(R.id.restartButton);
-        confirm = findViewById(R.id.confirmationButton);
-        buttons[0] = findViewById(R.id.char1);
-        buttons[1] = findViewById(R.id.char2);
-        buttons[2] = findViewById(R.id.char3);
-        buttons[3] = findViewById(R.id.char4);
-        buttons[4] = findViewById(R.id.char5);
-        buttons[5] = findViewById(R.id.char6);
-        buttons[6] = findViewById(R.id.char7);
-        buttons[7] = findViewById(R.id.char8);
-        buttons[8] = findViewById(R.id.char9);
-        buttons[9] = findViewById(R.id.char10);
-        buttons[10] = findViewById(R.id.char11);
-        buttons[11] = findViewById(R.id.char12);
-
-
-        Collections.shuffle(mots);
+        super.onCreate(savedInstanceState);       //lancement de l'activité
+        setContentView(R.layout.game_1);          //on choisit l'interface graphique de l'activité
 
         indice =0;
         score =0;
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = preferences.edit();
-        float temp_1 = preferences.getFloat("HighScore",0);
-        editor.apply();
-        affichageHighscore.setText(String.valueOf(temp_1));
-        updateView(mots.get(getNewWord()));
+        initialisationEtMelangeListes();          //récupération des listes de mots et mélange de la liste de mots
+        initialisationElementGraphique();
+        recupererHighScore();                     //récupération du highscore stocker en preferences
 
-        for (int j = 0; j < 12; j++){
+
+        updateView(mots.get(getNewWord()));       //maj IHM
+
+        for (int j = 0; j < 12; j++){             //mise en fonctionnement des boutons
             buttons[j].setOnClickListener(clickListenerButton);
         }
 
         restart.setOnClickListener(clickListenerRestart);
         confirm.setOnClickListener(clickListenerValidate);
 
-
-        new CountDownTimer(101000, 1000) {
+        new CountDownTimer(101000, 1000) {                      //compte à rebours du jeu
 
             public void onTick(long millisUntilFinished) {
-                chronometer.setText(Long.toString(millisUntilFinished / 1000));
+                chronometer.setText(Long.toString(millisUntilFinished / 1000));         //on affiche le temps restant chaque seconde
             }
 
             public void onFinish() {
-                chronometer.setText("0");
+                chronometer.setText("0");                                                 //actions effectuées à l'arrêt du chrono
                 String toastTextFin = "Temps écoulé !\nVotre score est de " + score;
                 toast = Toast.makeText(getBaseContext(), toastTextFin, Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
@@ -128,20 +90,12 @@ public class GameOneActivity extends Activity {
                         finish();
                     }
                 }, 4000);
-
             }
-
         }.start();
-
-
-
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    private void updateNewRandomWord(Mots mots) {
-        motFr.setText(mots.getMotsFr());
-    }
 
     private String shuffleWord (String word) {
         List<Character> l = new ArrayList<>();
@@ -157,30 +111,29 @@ public class GameOneActivity extends Activity {
         return word;
     }
 
+    private void updateView (Mots mots){  //permet de rafraichir ce qu'on voit à l'écran
 
-    private void updateView (Mots mots){
-
-        updateNewRandomWord(mots);
+        motFr.setText(mots.getMotsFr());
         affichageScore.setText(String.valueOf(score));
         String motMelange = shuffleWord(mots.getMotsEn());
 
         int i = 0;
 
-        for (int x = 0; x < motMelange.length(); x++){
+        for (int x = 0; x < motMelange.length(); x++){       //génération lettres du mot sur boutons
             buttons[x].setText(Character.toString(motMelange.charAt(x)));
             i++;
         }
         while(i<12) {
-            Random r = new Random();
+            Random r = new Random();                        //génération lettre sur boutons restants
             char c = (char)(r.nextInt(26) + 'a');
             buttons[i].setText(Character.toString(c));
             i++;
         }
-    }
+    };
 
     private View.OnClickListener clickListenerButton = new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
+        public void onClick(View v) {       //actions au toucher des lettres
             Button boutonCliqué = (Button) v;
             String lettreBouton = boutonCliqué.getText().toString();
             String temp = motADeviner.getText().toString();
@@ -191,7 +144,7 @@ public class GameOneActivity extends Activity {
 
     private View.OnClickListener clickListenerRestart = new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
+        public void onClick(View v) {      //actions au toucher de restart
             motADeviner.setText("");
             for (int k = 0; k < 12; k++){
                 buttons[k].setVisibility(View.VISIBLE);
@@ -199,14 +152,14 @@ public class GameOneActivity extends Activity {
         }
     };
 
-    private int getNewWord(){
+    private int getNewWord(){                                        //récupérer nouveau mot anglais
         String test = new String();
         do {
             indice++;
             test = mots.get(indice).getMotsEn();
         }while (test.length() > 11);
         return (indice);
-    };
+    }
 
     private View.OnClickListener clickListenerValidate = new View.OnClickListener() {
         @Override
@@ -214,7 +167,7 @@ public class GameOneActivity extends Activity {
             valide();
         }
     };
-
+                                                                                    //actions au toucher de restart
     private void valide(){
         String motSolution = mots.get(indice).getMotsEn();
         if (motADeviner.getText().toString().equals(motSolution)) {
@@ -226,9 +179,9 @@ public class GameOneActivity extends Activity {
             onWrongAnswer();
         }
         updateView(mots.get(getNewWord()));
-    };
+    }
 
-    private void onRightAnswer(){
+    private void onRightAnswer(){                                                  //actions lors d'une bonne réponse
         toast = Toast.makeText(getBaseContext(), "Bonne réponse", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
@@ -238,7 +191,7 @@ public class GameOneActivity extends Activity {
             buttons[k].setVisibility(View.VISIBLE);}
     }
 
-    private void onWrongAnswer(){
+    private void onWrongAnswer(){                                                 //actions lors d'une mauvaise réponse
         String toastText = "Réponse incorrecte, la(les) réponses\ncorrectes étaient :";
         int i;
         for (i = 0; i < mots.get(indice).getTabMotsEn().length; i++){
@@ -254,14 +207,14 @@ public class GameOneActivity extends Activity {
             buttons[k].setVisibility(View.VISIBLE);}
     }
 
-    private void disableButtons(){
+    private void disableButtons(){                                              //désactivation des boutons à la fin du chrono
         for (int x = 0; x < 12; x++){
             buttons[x].setEnabled(false);}
         restart.setEnabled(false);
         confirm.setEnabled(false);
     }
 
-    private void keepHighscore() {
+    private void keepHighscore() {                                              //garder le highscore entre 2 parties et affichage nouveau meilleur score
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = preferences.edit();
         float temp = preferences.getFloat("HighScore", 0);
@@ -276,7 +229,7 @@ public class GameOneActivity extends Activity {
         }
     }
 
-    private void reduceToastTimeAppearing () {
+    private void reduceToastTimeAppearing () {          //réduire le temps d'apparition des toasts
         Handler handler_2 = new Handler();
         handler_2.postDelayed(new Runnable() {
             @Override
@@ -285,4 +238,53 @@ public class GameOneActivity extends Activity {
             }
         }, 1100);
     }
+
+    private void initialisationEtMelangeListes() {
+
+        listesDAO = AppDataBase.getAppDatabase(getApplicationContext()).ListesDAO();
+        nomListe = findViewById(R.id.ListName);
+        Bundle extras = getIntent().getExtras();
+        idList = listesDAO.idDeListe((extras.getString("nameList")));
+        nomListe.setText(listesDAO.getProperName(idList));
+        motsDAO = AppDataBase.getAppDatabase(getApplicationContext()).MotsDao();
+        mots = motsDAO.getList(idList);
+
+
+        Collections.shuffle(mots);
     }
+
+    private void initialisationElementGraphique() {
+
+        motADeviner = findViewById(R.id.motadeviner);
+        motADeviner.setText("");
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setText("c");
+
+        affichageScore = findViewById(R.id.score);
+        affichageHighscore = findViewById(R.id.highscore);
+        motFr = findViewById(R.id.motFr);
+        restart = findViewById(R.id.restartButton);
+        confirm = findViewById(R.id.confirmationButton);
+        buttons[0] = findViewById(R.id.char1);
+        buttons[1] = findViewById(R.id.char2);
+        buttons[2] = findViewById(R.id.char3);
+        buttons[3] = findViewById(R.id.char4);
+        buttons[4] = findViewById(R.id.char5);
+        buttons[5] = findViewById(R.id.char6);
+        buttons[6] = findViewById(R.id.char7);
+        buttons[7] = findViewById(R.id.char8);
+        buttons[8] = findViewById(R.id.char9);
+        buttons[9] = findViewById(R.id.char10);
+        buttons[10] = findViewById(R.id.char11);
+        buttons[11] = findViewById(R.id.char12);
+    }
+
+    private void recupererHighScore() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        float temp_1 = preferences.getFloat("HighScore",0);
+        editor.apply();
+        affichageHighscore.setText(String.valueOf(temp_1));
+    }
+}
